4c16a793ddac724aa574f585a9745058f53853af
prolletto64 <poiluonzo@mail.com>
2024-08-19 21:17:38 +0200

    ui is editable only for authenticated authors

--- src/app/note/[id]/page.tsx
+++ src/app/note/[id]/page.tsx
@@ -1,17 +1,36 @@
+import { currentUser } from "@clerk/nextjs/server";
 import { getSingleNote } from "~/db_queries";
 
 export default async function NotePage({ params }: { params: { id: number } }) {
+  const user = await currentUser();
+  const note = await getSingleNote(params.id);
   return (
     <div>
-      <form action="/updatepost" method="post">
-      <textarea className="m-4 h-96 w-96 bg-yellow-300 p-4 text-black" name="note-text" id="note-text">
-        {(await getSingleNote(params.id))!.text}
-      </textarea>
-      <div><input type="submit" value="update" /></div>
-      </form>
-      <form action="/deletepost" method="post">
-      <input type="submit" value="delete" />
-      </form>
+      {note!.author == user!.username && (
+        <div>
+          <form action="/updatepost" method="post">
+            <textarea
+              className="m-4 h-96 w-96 bg-yellow-300 p-4 text-black"
+              name="note-text"
+              id="note-text"
+            >
+              {note!.text}
+            </textarea>
+            <input type="hidden" name="id" value={params.id} />
+            <div>
+              <input type="submit" value="update" />
+            </div>
+          </form>
+          <form action="/deletepost" method="post">
+            <input type="submit" value="delete" />
+          </form>
+        </div>
+      )}
+      {note!.author != user!.username && (
+        <div className="m-4 h-96 w-96 bg-yellow-300 p-4 text-black">
+          {note!.text}
+        </div>
+      )}
     </div>
   );
 }
